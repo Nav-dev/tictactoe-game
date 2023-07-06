316de0d7b0fe3a329a9cf6022b40839a5ca898dc
Nav-dev <sharmanav325@gmail.com>
2023-07-06 13:21:20 +0530

    display X when clicking

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    console.log(squares);
+
+    const handleSquareClick = clickedposition => {
+
+        setSquares((currentSquares) =>{
+            return currentSquares.map((squareValue, position)=>{
+
+                if(clickedposition === position){
+                    return 'X';
+                }
+                return squareValue;
+            })
+        })
+
+    };
+
+    const renderSquare = position => {
+        return (
+            <Square
+                value={squares[position]}
+                onClick={()=> handleSquareClick(position)}
+            />
+        );
+    };
   return (
     <div className="board">
-      <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+      <div className="board-row"> 
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
