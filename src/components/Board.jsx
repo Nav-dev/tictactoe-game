61d4f8804a6272c0f1d5e55d0898fddfd1ca58fb
Nav-dev <sharmanav325@gmail.com>
2023-07-06 15:44:32 +0530

    showed the winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,6 @@
-import { useState } from 'react';
 import Square from './Square';
 
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(false);
-
-    const handleSquareClick = clickedposition => {
-
-        if(squares[clickedposition]){
-            return;
-        }
-        setSquares((currentSquares) =>{
-            return currentSquares.map((squareValue, position)=>{
-
-                if(clickedposition === position){
-                    return isXNext ? 'X':'O';
-                }
-                return squareValue;
-            })
-        });
-
-        setIsXNext((currentIsXNext)=>!currentIsXNext)
-    };
+const Board = ({ squares, handleSquareClick}) => {
 
     const renderSquare = position => {
         return (
