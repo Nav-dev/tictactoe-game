76b8e344af0489906bd91d55c571e277cf91beae
Nav-dev <sharmanav325@gmail.com>
2023-07-08 23:25:02 +0530

    added bg circles

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,7 @@ function App() {
       Current Game History
       </h2>
         <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
       </div>
     
   );
